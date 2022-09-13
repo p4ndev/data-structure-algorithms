80e69582e3a5855fcb0cb6e796ad934385aa4cc1
DESKTOP-VB4R1O3\Developer <gustavo_hen@hotmail.com>
2022-09-13 19:54:54 -0300

    + Efficient O(n log n)

---
+++ FindMissing_Number_Efficient.cs
@@ -0,0 +1,14 @@
+﻿var arr = new int[]{ 1,2,4,5,6 };  // Ouput: 3
+
+Console.WriteLine(
+    FindMissing_Number_Efficient(arr, arr.Length)
+);
+
+static int FindMissing_Number_Efficient(int[] a, int n) {
+    Array.Sort(a);
+    for (int i = 0, t = 0; i < (n - 1); i++){
+        t = (a[i] + 1);
+        if (t != a[i + 1]) return t;
+    }
+    return Int32.MaxValue;
+}
