c0f41cf1ddb0d20d795b331b3dafca54fbb3f5b7
DESKTOP-VB4R1O3\Developer <gustavo_hen@hotmail.com>
2022-10-09 10:37:21 -0300

    + Native Binary Search + Iterative Binary Search + Recursive Binary Search

--- BinarySearch.cs
+++ BinarySearch.cs
@@ -6,49 +6,33 @@ Console.WriteLine(IterativeBinarySearch(inp, fou));
 Console.WriteLine(RecursiveBinarySearch(inp, fou, 0, (inp.Count - 1)));
 
 int NativeBinarySearch(IList<int> data, int num){
-
     var result = Array.BinarySearch(data.ToArray(), num);
-
     return result < 0 ? -1 : result;
-
 }
 
-int IterativeBinarySearch(IList<int> data, int num) {
-    
+int IterativeBinarySearch(IList<int> data, int num) {    
     int min = 0, max = (data.Count - 1);
-
     while (min <= max) {
-
         int mid = ((min + max) / 2);
-
         if (num.Equals(data[mid]))
             return mid;
         else if (num < data[mid])
             max = mid - 1;
         else
             min = mid + 1;
-
     }
-
     return -1;
-
 }
 
 int RecursiveBinarySearch(IList<int> data, int num, int min, int max) {
-
     if (min <= max){
-
         int mid = ((min + max) / 2);
-
         if (num.Equals(data[mid]))
             return mid;
         else if (num < data[mid])
             return RecursiveBinarySearch(data, num, min, mid - 1);
         else
             return RecursiveBinarySearch(data, num, mid + 1, max);
-
     }
-
-    return -1;
-    
+    return -1;    
 }
