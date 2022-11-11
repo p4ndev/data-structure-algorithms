27e0d35be75e4b856640685a4d54f6ff94ae0ad7
DESKTOP-VB4R1O3\Developer <gustavo_hen@hotmail.com>
2022-11-10 21:15:00 -0300

    * First try I had 75% Success and 25% Fail * Refactoring and creating another version

--- BiggerIsGreater/Program.cs
+++ BiggerIsGreater/Program.cs
@@ -1,8 +1,2 @@
-﻿using static hacker_rank.AlgorithmImplementation;
-
-string inp = "lmno", exp = "lmon";
-//string inp = "abdc", exp = "acbd";
-//string inp = "ab", exp = "ba";
-//string inp = "fedcbabcd", exp = "fedcbabdc";
-
-Console.WriteLine("{0}\n{1}",BiggerIsGreater(inp),exp);
+﻿
+Console.WriteLine("Hello, World!");
