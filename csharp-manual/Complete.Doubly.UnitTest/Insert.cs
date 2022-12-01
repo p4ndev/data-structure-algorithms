41ff00796b9f28086df062ba8c83cddea1a86bf6
DESKTOP-VB4R1O3\Developer <gustavo_hen@hotmail.com>
2022-12-01 20:31:02 -0300

    * Fix files and folders

--- csharp-manual/Complete.Doubly.UnitTest/Insert.cs
+++ csharp-manual/Complete.Doubly.UnitTest/Insert.cs
@@ -1,6 +1,6 @@
 namespace Complete.Doubly.UnitTest.Insert;
 
-public class AddFirst{
+public class Insert{
 
     private int _begin, _end;
     private LinkedNodeList? _sut;
@@ -58,6 +58,65 @@ public class AddFirst{
 
         // Assert
         foreach (var node in _sut.MoveNext()){
+            Assert.Equal(arr[idx], node.Data);
+            idx++;
+        }
+
+    }
+
+    [Fact]
+    public void UT004_AddLast_Empty(){
+
+        // Arrange
+        _sut = new();
+        _begin = _end = 0;
+
+        // Act
+        _begin = _sut.Count;
+        _sut.AddLast(10);
+        _end = _sut.Count;
+
+        // Assert
+        Assert.Equal(0, _begin);
+        Assert.Equal(1, _end);
+
+    }
+
+    [Fact]
+    public void UT005_AddLast_Multiple(){
+
+        // Arrange
+        _sut = new();
+        _begin = _end = 0;
+
+        // Act
+        _begin = _sut.Count;
+        _sut.AddLast(10);
+        _sut.AddLast(20);
+        _sut.AddLast(30);
+        _end = _sut.Count;
+
+        // Assert
+        Assert.Equal(0, _begin);
+        Assert.Equal(3, _end);
+
+    }
+
+    [Fact]
+    public void UT006_AddLast_Data(){
+
+        // Arrange
+        int idx = 0;
+        _sut = new();
+        var arr = new[] { 30, 20, 10 };
+
+        // Act
+        _sut.AddLast(arr[idx + 2]); // 10
+        _sut.AddLast(arr[idx + 1]); // 20
+        _sut.AddLast(arr[idx]);     // 30
+
+        // Assert
+        foreach (var node in _sut.MovePrevious()){
             Assert.Equal(arr[idx], node.Data);
             idx++;
         }
