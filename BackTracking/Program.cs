e27ff16ca9bab900c63f7bc6ea571b610bc885a0
DESKTOP-VB4R1O3\Developer <gustavo_hen@hotmail.com>
2022-12-13 12:59:12 -0300

    + Statement

--- BackTracking/Program.cs
+++ BackTracking/Program.cs
@@ -1,32 +1,55 @@
 ﻿using Maze;
 
+/* ================= I/P (tree) =================
+
+                0
+             /     \
+            /       \
+           1         3
+            \       / \
+             2     4   6
+                  / \
+                 5   7
+                      \
+                       8
+
+=============== Maze (abstract) ===================
+
+            |------------------------|
+  start =>     0       1      2      |
+                  -------------------|
+            |  3       4      5      |
+            |     |        ----------|
+            |  6  |    7      8        ===> End
+            |-----|------------------|
+
+========================= O/P =====================
+
+    [ 0, 3, 4, 7, 8 ]
+    from start to end
+    best solution
+
+**************************************************/
+
 var mz = new MazeTree();
 var bt = mz.Build();
 
-if (bt.Root is not null) {
-    var rt = new RegularTraversal(bt, 0, 8);
-    var zero    = rt.Find(0);
-    var four    = rt.Find(4);
-    var six     = rt.Find(6);
-    var eight   = rt.Find(8);
-    var twenty  = rt.Find(20);
-}
+// Simplest way to find nodes recursively
+var rt      = new RegularTraversal(bt, 0, 8);
+var zero    = rt.Find(0);
+var six     = rt.Find(6);
+var twenty  = rt.Find(20);
 
-IEnumerable<int>? res;
+// Based on children focused on math operation
+var mst = new MathSiblingsTraversal(bt, 0, 8);
+var re0 = mst.Find();
 
-//var mst = new MathSiblingsTraversal(bt, 0, 8);
-//res = mst.Find();
-//if (res is not null)
-//    Console.WriteLine(String.Join(",", res));
+// Backtracking based on availability
+var mmt = new MinMaxTraversal(bt, 0, 8);
+var re1 = mmt.Find();
 
-//var mmt = new MinMaxTraversal(bt, 0, 8);
-//res = mmt.Find();
-//if (res is not null)
-//    Console.WriteLine(String.Join(",", res));
-
-//var sst = new StateSpaceTree(bt, 0, 8);
-//res = sst.Find();
-//if (res is not null)
-//    Console.WriteLine(String.Join(",", res));
+// Every single possibilities first, and then best
+var sst = new StateSpaceTree(bt, 0, 8);
+var re2 = sst.Find();
 
 Console.WriteLine("Done...");
