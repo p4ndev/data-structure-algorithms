ca163477debb86249a69daad50a296e9e6daee7f
Gustavo Henrique <gustavo_hen@hotmail.com>
2026-07-06 13:48:11 -0300

    Complete and passed

--- runnable/Program.cs
+++ runnable/Program.cs
@@ -1,31 +1,125 @@
-﻿string input = "1-(     -2)"; // OUTPUT (Incorrect): -1
+﻿string input = "1-(     -2)";
 int output = Calculate(input), expectation = 3;
 
-//string input = "(-2+3)"; // OUTPUT (Incorrect): -1
-//int output = Calculate(input), expectation = 1;
-
-//string input = "(-2-3)"; // OUTPUT (Incorrect): -5
-//int output = Calculate(input), expectation = -5;
-
-//string input = "1+(-2)"; // OUTPUT (Incorrect): 3
-//int output = Calculate(input), expectation = -1;
-
-//string input = "-(2+3)"; // OUTPUT (Incorrect): 5
-//int output = Calculate(input), expectation = -5;
-
-//string input = "-(1-(2+3))"; // OUTPUT (Incorrect): -4
-//int output = Calculate(input), expectation = 4;
-
-//string input = "-2147483647"; // OUTPUT (Incorrect): 2147483647
-//int output = Calculate(input), expectation = -2147483647;
-
-//string input = "0-(-1)"; // OUTPUT (Incorrect): -1
-//int output = Calculate(input), expectation = 1;
-
-//string input = "(-123)"; // OUTPUT (Incorrect): 123
-//int output = Calculate(input), expectation = -123;
-
+Console.ForegroundColor = expectation.Equals(output) ? ConsoleColor.Green : ConsoleColor.Red;
 Console.WriteLine("Input: {0}\nOutput: {1}\nExpectation: {2}", input, output, expectation);
+Console.WriteLine();
+
+input = "1+(-2)";
+output = Calculate(input); expectation = -1;
+
+Console.ForegroundColor = expectation.Equals(output) ? ConsoleColor.Green : ConsoleColor.Red;
+Console.WriteLine("Input: {0}\nOutput: {1}\nExpectation: {2}", input, output, expectation);
+Console.WriteLine();
+
+input = "0-(-1)";
+output = Calculate(input); expectation = 1;
+
+Console.ForegroundColor = expectation.Equals(output) ? ConsoleColor.Green : ConsoleColor.Red;
+Console.WriteLine("Input: {0}\nOutput: {1}\nExpectation: {2}", input, output, expectation);
+Console.WriteLine();
+
+input = "-(1-(2+3))";
+output = Calculate(input); expectation = 4;
+
+Console.ForegroundColor = expectation.Equals(output) ? ConsoleColor.Green : ConsoleColor.Red;
+Console.WriteLine("Input: {0}\nOutput: {1}\nExpectation: {2}", input, output, expectation);
+Console.WriteLine();
+
+input = "-(2+3)";
+output = Calculate(input); expectation = -5;
+
+Console.ForegroundColor = expectation.Equals(output) ? ConsoleColor.Green : ConsoleColor.Red;
+Console.WriteLine("Input: {0}\nOutput: {1}\nExpectation: {2}", input, output, expectation);
+Console.WriteLine();
+
+input = "(-2+3)";
+output = Calculate(input); expectation = 1;
+
+Console.ForegroundColor = expectation.Equals(output) ? ConsoleColor.Green : ConsoleColor.Red;
+Console.WriteLine("Input: {0}\nOutput: {1}\nExpectation: {2}", input, output, expectation);
+Console.WriteLine();
+
+input = "(-2-3)";
+output = Calculate(input); expectation = -5;
+
+Console.ForegroundColor = expectation.Equals(output) ? ConsoleColor.Green : ConsoleColor.Red;
+Console.WriteLine("Input: {0}\nOutput: {1}\nExpectation: {2}", input, output, expectation);
+Console.WriteLine();
+
+input = "-2147483647";
+output = Calculate(input); expectation = -2147483647;
+
+Console.ForegroundColor = expectation.Equals(output) ? ConsoleColor.Green : ConsoleColor.Red;
+Console.WriteLine("Input: {0}\nOutput: {1}\nExpectation: {2}", input, output, expectation);
+Console.WriteLine();
+
+input = "(-123)";
+output = Calculate(input); expectation = -123;
+
+Console.ForegroundColor = expectation.Equals(output) ? ConsoleColor.Green : ConsoleColor.Red;
+Console.WriteLine("Input: {0}\nOutput: {1}\nExpectation: {2}", input, output, expectation);
+Console.WriteLine();
+
+input = "1 + 1";
+output = Calculate(input); expectation = 2;
+
+Console.ForegroundColor = expectation.Equals(output) ? ConsoleColor.Green : ConsoleColor.Red;
+Console.WriteLine("Input: {0}\nOutput: {1}\nExpectation: {2}", input, output, expectation);
+Console.WriteLine();
+
+input = "(1+(4+5+2)-3)+(6+8)";
+output = Calculate(input); expectation = 23;
+
+Console.ForegroundColor = expectation.Equals(output) ? ConsoleColor.Green : ConsoleColor.Red;
+Console.WriteLine("Input: {0}\nOutput: {1}\nExpectation: {2}", input, output, expectation);
+Console.WriteLine();
+
+input = " 2-1 + 2 ";
+output = Calculate(input); expectation = 3;
+
+Console.ForegroundColor = expectation.Equals(output) ? ConsoleColor.Green : ConsoleColor.Red;
+Console.WriteLine("Input: {0}\nOutput: {1}\nExpectation: {2}", input, output, expectation);
+Console.WriteLine();
+
+input = " 2-1 + 2 - 5 + 20 - 5 ";
+output = Calculate(input); expectation = 13;
+
+Console.ForegroundColor = expectation.Equals(output) ? ConsoleColor.Green : ConsoleColor.Red;
+Console.WriteLine("Input: {0}\nOutput: {1}\nExpectation: {2}", input, output, expectation);
+Console.WriteLine();
+
+input = "(1+(4+5+2)-3)+(6+8)";
+output = Calculate(input); expectation = 23;
+
+Console.ForegroundColor = expectation.Equals(output) ? ConsoleColor.Green : ConsoleColor.Red;
+Console.WriteLine("Input: {0}\nOutput: {1}\nExpectation: {2}", input, output, expectation);
+Console.WriteLine();
+
+input = "(1+(4+5+2)-3)+(6+8)";
+output = Calculate(input); expectation = 23;
+
+Console.ForegroundColor = expectation.Equals(output) ? ConsoleColor.Green : ConsoleColor.Red;
+Console.WriteLine("Input: {0}\nOutput: {1}\nExpectation: {2}", input, output, expectation);
+Console.WriteLine();
+
+input = "-2147483648";
+output = Calculate(input); expectation = -2147483648;
+
+Console.ForegroundColor = expectation.Equals(output) ? ConsoleColor.Green : ConsoleColor.Red;
+Console.WriteLine("Input: {0}\nOutput: {1}\nExpectation: {2}", input, output, expectation);
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.White;
+
+/***********************************************************************************************************************/
+/***********************************************************************************************************************/
+/***********************************************************************************************************************/
+
+// Time     : 5 hours
+// Runtime  : 49 ms         Beats   : 6.67%
+// Memory   : 54.78 Mb      Beats   : 8.33%
+// https://leetcode.com/problems/basic-calculator/submissions/2058302278
 
 /***********************************************************************************************************************/
 /***********************************************************************************************************************/
@@ -39,56 +133,53 @@ void PersistPendingData(Stack<string> storage, System.Text.StringBuilder sb) {
     sb.Clear();
 }
 
-void FillOperations(Stack<string> storage, Queue<int> numbers, Queue<bool> sumOperations)
+void SolveOperations(Stack<string> storage, Stack<long> numbers)
 {
-    while (storage.TryPop(out string? term) && !term.Equals(")"))
+    Stack<bool> sumOperations = new();
+    long num = 0, acc = 0, tmp = 0;
+    numbers.Clear();
+    string term;
+
+    #pragma warning disable CS8600
+    while (storage.TryPop(out term) && !term.Equals(")"))
+    {
         switch (term)
         {
-            case "+":   sumOperations.Enqueue(true);                break;
-            case "-":   sumOperations.Enqueue(false);               break;
-            default:    numbers.Enqueue(Convert.ToInt32(term));     break;
+            case "+":
+                sumOperations.Push(true);
+                break;
+
+            case "-":
+                sumOperations.Push(false);
+                break;
+
+            default:
+                num = Convert.ToInt64(term);
+                
+                if (sumOperations.Count > 0 && !sumOperations.Pop())
+                    num *= -1;
+
+                numbers.Push(num);
+                break;
         }
+    }
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+    while (numbers.TryPop(out tmp))
+        acc += tmp;
+
+    storage.Push(acc.ToString());
 }
-
-void SolveOperations(Stack<string> storage, Queue<int> numbers, Queue<bool> sumOperations)
-{
-    bool isSum;
-    int left = numbers.Dequeue(), right;
-    
-    while(numbers.TryDequeue(out right))
-        if(sumOperations.TryDequeue(out isSum))
-            switch (isSum)
-            {
-                case true:
-                    left += right;
-                    break;
-
-                case false:
-                    left -= right;
-                    break;
-            }
-
-    storage.Push(left.ToString());
-}
-
-/***********************************************************************************************************************/
-/***********************************************************************************************************************/
-/***********************************************************************************************************************/
 
 int Calculate(string s)
 {
     if (String.IsNullOrWhiteSpace(s))
         return default;
 
-    #region Declaration
-    char input;
-
     System.Text.StringBuilder sb = new();
     Stack<string> storage = new();
-
-    Queue<int> numbers = new();
-    Queue<bool> sumOperations = new();
-    #endregion
+    Stack<long> numbers = new();
+    char input;
 
     for (int i = (s.Length - 1); i >= 0; i--)
     {
@@ -112,12 +203,8 @@ int Calculate(string s)
                 break;
 
             case '(':
-                numbers.Clear();
-                sumOperations.Clear();
-
                 PersistPendingData(storage, sb);
-                FillOperations(storage, numbers, sumOperations);
-                SolveOperations(storage, numbers, sumOperations);
+                SolveOperations(storage, numbers);
                 break;
         }
     }
@@ -125,13 +212,7 @@ int Calculate(string s)
     PersistPendingData(storage, sb);
 
     if (storage.Count > 1)
-    {
-        numbers.Clear();
-        sumOperations.Clear();
+        SolveOperations(storage, numbers);
 
-        FillOperations(storage, numbers, sumOperations);
-        SolveOperations(storage, numbers, sumOperations);
-    }
-    
     return Convert.ToInt32(storage.Pop());
 }
