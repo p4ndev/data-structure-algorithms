220f511664a91e992c5c2e227d1403371fd07b62
Gustavo Henrique <gustavo_hen@hotmail.com>
2026-07-10 22:16:40 -0300

    LC1472 WIP

---
+++ Problems/LC1472BrowserHistory.cs
@@ -0,0 +1,33 @@
+﻿namespace Problems;
+
+public class LC1472
+{
+    private string _tmp;
+
+    public LC1472(string homepage)
+    {
+        _tmp = homepage;
+    }
+
+    public void Visit(string url)
+    {
+        
+    }
+
+    public string Back(int steps)
+    {
+        return _tmp;
+    }
+
+    public string Forward(int steps)
+    {
+        return _tmp;
+    }
+}
+
+/*
+ LC1472 obj = new LC1472(homepage);
+ obj.Visit(url);
+ string param_2 = obj.Back(steps);
+ string param_3 = obj.Forward(steps);
+*/
