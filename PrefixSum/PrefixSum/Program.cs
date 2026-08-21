1aa3eed4d5d0a0307ec2d0b0039482beba266bd8
Gustavo Henrique <gustavo_hen@hotmail.com>
2026-08-21 18:16:20 -0300

    https://www.geeksforgeeks.org/dsa/find-original-array-from-given-prefix-sum-array/

---
+++ PrefixSum/PrefixSum/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] presum = new int[] { 45, 57, 63, 78, 89, 97 };
+int[] original = new int[] { 45, 12, 6, 15, 11, 8 };
+
+int[] arr = DecodeArray(presum);
+
+Console.WriteLine(string.Join(",", presum));
+Console.WriteLine(string.Join(",", original));
+
+static int[] DecodeArray(int[] presum)
+{
+    int i = (presum.Length - 1), o;
+
+    while(i >= 0)
+    {
+        if (i == 0) break;
+
+        o = presum[i - 1];
+        presum[i] -= o;
+        i--;
+    }
+
+    return presum;
+}
