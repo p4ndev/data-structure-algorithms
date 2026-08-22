dfc2c493bae227cf7492dddc206fdeded8b7be47
Gustavo Henrique <gustavo_hen@hotmail.com>
2026-08-22 10:34:33 -0300

    https://www.geeksforgeeks.org/dsa/mean-range-array/

---
+++ PrefixSum/PrefixSum/Mean.cs
@@ -0,0 +1,56 @@
+﻿public static class Mean
+{
+    public static List<int> Find(int[] arr, int[][] queries)
+    {
+        // Prefix sum instead of a native approach
+        List<int> output = new();
+
+        // Possible side effect
+        if (arr.Length == 0 || queries.Length == 0)
+            return output;
+
+        for (int i = 1; i < arr.Length; i++)
+            arr[i] += arr[i - 1];
+
+        int l, r, e, c;
+        foreach (int[] q in queries)
+        {
+            l = q[0];
+            r = q[1];
+
+            e = ((r + 1) - l);
+            
+            c = arr[r];
+            if (l != 0)
+                c -= arr[l - 1];
+
+            c /= e;
+
+            output.Add(c);
+        }
+
+        return output;
+    }
+
+    /*
+    var output = new List<int>();
+
+    int t, o, l, r;
+    foreach (int[] q in queries)
+    {
+        t = o = 0;
+        l = q[0];
+        r = q[1];
+
+        while(l <= r)
+        {
+            t += arr[l++];
+            o++;
+        }
+
+        output.Add(t / o);
+    }
+
+    return output; 
+    */
+}
--- PrefixSum/PrefixSum/Program.cs
+++ PrefixSum/PrefixSum/Program.cs
@@ -12,17 +12,17 @@
 //bool expected = false;
 //int[] arr = { 1, 1, 1, 1, 1 };
 
-bool expected = true;
-int[] arr = { 6, 3, 2, 1 };
+//bool expected = true;
+//int[] arr = { 6, 3, 2, 1 };
 
 //bool expected = false;
 //int[] arr = { 100, 1, 1, 1, 1, 1 };
 
-bool result = Split.IsAbleTo(arr);
+//bool result = Split.IsAbleTo(arr);
 
-Console.ForegroundColor = (result == expected ? ConsoleColor.Green : ConsoleColor.Red);
-Console.WriteLine("The array items [{0}] is {1}able to split.", string.Join(",", arr), (result ? "" : "not "));
-Console.ResetColor();
+//Console.ForegroundColor = (result == expected ? ConsoleColor.Green : ConsoleColor.Red);
+//Console.WriteLine("The array items [{0}] is {1}able to split.", string.Join(",", arr), (result ? "" : "not "));
+//Console.ResetColor();
 
 
 
@@ -60,16 +60,63 @@ Console.ResetColor();
 
 
 
-// [   ] https://www.geeksforgeeks.org/dsa/mean-range-array/
+// [ Y ] https://www.geeksforgeeks.org/dsa/mean-range-array/
 
 //int[] arr = { 3, 7, 2, 8, 2 };
 //List<int> expected = new() { 5, 5, 4 };
 //int[][] queries = { [0, 1], [1, 3], [2, 4] };
 
+//int[] arr = { 10, 20, 30, 40, 50, 60 };
+//List<int> expected = new() { 50 };
+//int[][] queries = { [3, 5] };
+
+//int[] arr = { 1, 2, 3, 4, 5 };
+//List<int> expected = new() { 1, 2, 3 };
+//int[][] queries = { [0, 0], [1, 1], [2, 2] };
+
+//int[] arr = {
+//    81, 14, 3, 94, 35,
+//    31, 28, 17, 94, 13,
+//    86, 94, 69, 11, 75,
+//    54, 4, 3, 11, 27,
+//    29, 64, 77, 3, 71,
+//    25, 91, 83, 89, 69
+//};
+
+//List<int> expected = [
+//    81,
+//    43,
+//    41,
+//    48,
+//    41,
+//    51,
+//    43,
+//    46,
+//    47,
+//    43,
+//    69,
+//    44
+//];
+
+//int[][] queries = {
+//    [0, 0],
+//    [0, 5],
+//    [0, 9],
+//    [0, 29],
+//    [1, 10],
+//    [5, 14],
+//    [10, 19],
+//    [15, 29],
+//    [3, 17],
+//    [7, 23],
+//    [12, 12],
+//    [20, 25]
+//};
+
 //List<int> result = Mean.Find(arr, queries);
 
-//Console.WriteLine(string.Join(",", result));
-//Console.WriteLine(string.Join(",", expected));
+//Console.WriteLine("Result(s):\t\t{0}", string.Join(",", result));
+//Console.WriteLine("Expectation(s):\t\t{0}", string.Join(",", expected));
 
 
 
