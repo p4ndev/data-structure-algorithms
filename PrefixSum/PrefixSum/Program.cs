694f7353e3f2c5bd9fb7b4ddb7d0e54cb1559e28
Gustavo Henrique <gustavo_hen@hotmail.com>
2026-08-22 09:47:07 -0300

    https://www.geeksforgeeks.org/dsa/split-array-two-equal-sum-subarrays/

--- PrefixSum/PrefixSum/Program.cs
+++ PrefixSum/PrefixSum/Program.cs
@@ -1,23 +1,98 @@
-﻿int[] presum = new int[] { 45, 57, 63, 78, 89, 97 };
-int[] original = new int[] { 45, 12, 6, 15, 11, 8 };
+﻿// [ Y ] https://www.geeksforgeeks.org/dsa/split-array-two-equal-sum-subarrays/
 
-int[] arr = DecodeArray(presum);
+//bool expected = true;
+//int[] arr = { 1, 2, 3, 4, 5, 5 };
 
-Console.WriteLine(string.Join(",", presum));
-Console.WriteLine(string.Join(",", original));
+//bool expected = false;
+//int[] arr = { 4, 3, 2, 1 };
 
-static int[] DecodeArray(int[] presum)
-{
-    int i = (presum.Length - 1), o;
+//bool expected = true;
+//int[] arr = { 1, 2, 3 };
 
-    while(i >= 0)
-    {
-        if (i == 0) break;
+//bool expected = false;
+//int[] arr = { 1, 1, 1, 1, 1 };
 
-        o = presum[i - 1];
-        presum[i] -= o;
-        i--;
-    }
+bool expected = true;
+int[] arr = { 6, 3, 2, 1 };
 
-    return presum;
-}
+//bool expected = false;
+//int[] arr = { 100, 1, 1, 1, 1, 1 };
+
+bool result = Split.IsAbleTo(arr);
+
+Console.ForegroundColor = (result == expected ? ConsoleColor.Green : ConsoleColor.Red);
+Console.WriteLine("The array items [{0}] is {1}able to split.", string.Join(",", arr), (result ? "" : "not "));
+Console.ResetColor();
+
+
+
+
+
+
+
+
+
+
+
+
+// [   ] https://www.geeksforgeeks.org/dsa/equilibrium-index-of-an-array/
+
+//int expected = 2;
+//int[] arr = { 1, 2, 0, 3 };
+
+//int result = Equilibrium.Find(arr);
+
+//Console.WriteLine(string.Join(",", result));
+//Console.WriteLine(string.Join(",", expected));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// [   ] https://www.geeksforgeeks.org/dsa/mean-range-array/
+
+//int[] arr = { 3, 7, 2, 8, 2 };
+//List<int> expected = new() { 5, 5, 4 };
+//int[][] queries = { [0, 1], [1, 3], [2, 4] };
+
+//List<int> result = Mean.Find(arr, queries);
+
+//Console.WriteLine(string.Join(",", result));
+//Console.WriteLine(string.Join(",", expected));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// [ Y ] https://www.geeksforgeeks.org/dsa/find-original-array-from-given-prefix-sum-array/
+
+//int[] presum = new int[] { 45, 57, 63, 78, 89, 97 };
+//int[] expected = new int[] { 45, 12, 6, 15, 11, 8 };
+
+//int[] arr = Presum.DecodeArray(presum);
+
+//Console.WriteLine(string.Join(",", presum));
+//Console.WriteLine(string.Join(",", expected));
---
+++ PrefixSum/PrefixSum/Split.cs
@@ -0,0 +1,45 @@
+﻿public static class Split
+{
+    public static bool IsAbleTo(int[] arr)
+    {
+        // Don't store the prefix causing O(n) memory
+        int total = 0;
+        
+        foreach (int n in arr)
+            total += n;
+
+        if ((total % 2) != 0)
+            return false;
+
+        int leftSum = 0;
+        
+        for(int i = 0; i < arr.Length; i++)
+        {
+            leftSum += arr[i];
+
+            if (leftSum == (total - leftSum))
+                return true;
+        }
+
+        return false;
+    }
+
+    /*
+    int total, i;
+    var prefix = new int[arr.Length];
+        
+    prefix[0] = arr[0];
+    for (i = 1; i < arr.Length; i++)
+        prefix[i] = arr[i] + prefix[i - 1];
+    total = prefix[i - 1];
+
+    if ((total % 2) != 0)
+        return false;
+
+    for (i = 0; i < arr.Length; i++)
+        if (prefix[i] == (total - prefix[i]))
+            return true;
+
+    return false; 
+    */
+}
