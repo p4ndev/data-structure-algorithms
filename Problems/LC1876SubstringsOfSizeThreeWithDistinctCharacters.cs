795a2cb526147dc49410fa679cb91f53ea014ac9
Gustavo Henrique <gustavo_hen@hotmail.com>
2026-08-29 13:54:13 -0300

    Wip

--- Problems/LC1876SubstringsOfSizeThreeWithDistinctCharacters.cs
+++ Problems/LC1876SubstringsOfSizeThreeWithDistinctCharacters.cs
@@ -4,6 +4,35 @@ public class LC1876
 {
     public int CountGoodSubstrings(string s)
     {
-        return 0;
+        int k = 3, o = -1;
+
+        if (s.Length < 3)
+            return o;
+
+        int a = 0, p = 0;
+        char l = s[a];
+        int t = (l - 'a');
+
+        for (int i = 1; i < k; i++) {
+
+            if ((a + 1) > (s.Length - k))
+                return o;
+
+            p = (i + a);
+
+            if (l == s[p]) {
+                l = s[p];
+                t = (l - 'a');
+                a++;
+                i--;
+                continue;
+            }
+
+            l = s[p];
+            t += (l - 'a');
+        }
+
+
+        return o;
     }
 }
