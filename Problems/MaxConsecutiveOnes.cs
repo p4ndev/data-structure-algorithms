5fcfe4a0539f3f71f3071745a91dcf1590f68591
Gustavo Henrique <Gustavo_hen@hotmail.com>
2026-09-10 22:07:37 -0300

    Consecutives ones

---
+++ Problems/MaxConsecutiveOnes.cs
@@ -0,0 +1,20 @@
+﻿namespace Problems;
+
+public static class MaxConsecutiveOnes
+{
+    public static IEnumerable<int> Extract(int[] n)
+    {
+        int i = 0;
+
+        for (int j = 0; j < n.Length; j++)
+        {
+            if (n[j] == 1)
+            {
+                (n[i], n[j]) = (n[j], n[i]);
+                i++;
+            }
+        }
+
+        return n.Take(i);
+    }
+}
---
+++ TestCases/MaxConsecutiveOnesTests.cs
@@ -0,0 +1,54 @@
+﻿namespace MaxConsecutiveOnesTests;
+
+public class Scenarios
+{
+    [Theory]
+    [InlineData(
+        new[] { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 },
+        new[] { 1, 1, 1, 1, 1, 1, 1 }
+    )]
+    [InlineData(
+        new[] { 0, 0, 0, 0 },
+        new int[] { }
+    )]
+    [InlineData(
+        new[] { 1, 1, 1, 1 },
+        new[] { 1, 1, 1, 1 }
+    )]
+    [InlineData(
+        new[] { 0, 1, 0, 1, 0, 1 },
+        new[] { 1, 1, 1 }
+    )]
+    [InlineData(
+        new[] { 1, 0, 1, 0, 1, 0, 1 },
+        new[] { 1, 1, 1, 1 }
+    )]
+    [InlineData(
+        new[] { 0, 1, 1, 1, 0, 0 },
+        new[] { 1, 1, 1 }
+    )]
+    [InlineData(
+        new[] { 0, 0, 1, 1, 1, 1 },
+        new[] { 1, 1, 1, 1 }
+    )]
+    [InlineData(
+        new[] { 1, 1, 0, 0, 0, 1 },
+        new[] { 1, 1, 1 }
+    )]
+    [InlineData(
+        new[] { 0, 1, 0, 0, 1, 0, 0, 1 },
+        new[] { 1, 1, 1 }
+    )]
+    [InlineData(
+        new[] { 1, 0, 0, 0, 0, 0, 1 },
+        new[] { 1, 1 }
+    )]
+    public void Check(int[] n, IEnumerable<int> e)
+    {
+        // Act
+        IEnumerable<int> r = Problems.MaxConsecutiveOnes.Extract(n);
+
+        // Assert
+        Assert.Equal(e, r);
+    }
+}
