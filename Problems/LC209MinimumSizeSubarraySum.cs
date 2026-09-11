54ffff1b1121b1deb52616d664343708619c840e
Gustavo Henrique <gustavo_hen@hotmail.com>
2026-09-10 23:31:15 -0300

    Done

---
+++ Problems/LC209MinimumSizeSubarraySum.cs
@@ -0,0 +1,23 @@
+﻿namespace Problems;
+
+public class LC209
+{
+    public int MinSubArrayLen(int k, int[] a)
+    {
+        int c, i = 0, m = int.MaxValue, t = 0;
+
+        for (int j = 0; j < a.Length; j++)
+        {
+            t += a[j];
+
+            while (t >= k)
+            {
+                c = ((j - i) + 1);
+                m = Math.Min(m, c);
+                t -= a[i++];
+            }
+        }
+
+        return (m == int.MaxValue ? 0 : m);
+    }
+}
---
+++ TestCases/LC209MinimumSizeSubarraySumTests.cs
@@ -0,0 +1,27 @@
+﻿namespace LC209;
+
+public class LC209MinimumSizeSubarraySumTests
+{
+    private readonly Problems.LC209 _sut;
+
+    public LC209MinimumSizeSubarraySumTests()
+        => _sut = new();
+
+    [Theory]
+    [InlineData(7, new[] { 2, 3, 1, 2, 4, 3 }, 2)]
+    [InlineData(4, new[] { 1, 4, 4 }, 1)]
+    [InlineData(11, new[] { 1, 1, 1, 1, 1, 1, 1, 1 }, 0)]
+    [InlineData(1, new[] { 1 }, 1)]
+    [InlineData(5, new[] { 5 }, 1)]
+    [InlineData(6, new[] { 1, 2, 3 }, 3)]
+    [InlineData(15, new[] { 5, 1, 3, 5, 10, 7, 4, 9, 2, 8 }, 2)]
+    [InlineData(100, new[] { 1, 2, 3, 4, 5 }, 0)]
+    public void Should_Return_Minimum_Subarray_Length(int k, int[] n, int e)
+    {
+        // Act
+        int r = _sut.MinSubArrayLen(k, n);
+
+        // Assert
+        Assert.Equal(e, r);
+    }
+}
